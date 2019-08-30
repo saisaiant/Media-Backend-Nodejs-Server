a21fe4fb8a18c33da00d36310e29aacfaf1a0e0d
sai sai <saisaiant@gmail.com>
2019-08-30 17:10:10 +0630

    connect db

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
 require('dotenv').config();
 let express = require('express'),
     app = express();
+/*
+const cat = require('./database/cat');
+
+let catObj = {
+    id : 2,
+    name : 'Computer',
+    image : 'computer.png',
+    since : Date.now()
+}
+
+cat.save(catObj)
+.then(res => console.log(res))
+.catch(err => console.log(err));
+
+cat.all()
+.then(res => console.log(res))
+.catch(err => console.log(err));
+
+let obj = {
+    id : '5d68dc0c2622743d3ac5f470',
+    name : 'Car'
+}
+
+const seeder = require('./database/seeder');
+seeder.seedCat();
+
+cat.all().then(res => console.log(res)).catch(err => console.log(err));
+*/
+
 
 app.listen(process.env.PORT,()=>{
-    console.log('Server is running at ', process.env.PORT);
+    console.log('Server is running at', process.env.PORT);
 })
---
+++ database/db.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+let url = "mongodb://localhost:27017/mediaDB";
+const connect = mongoose.connect(url, {useNewUrlParser: true});
+const autoincrement  = require('mongoose-auto-increment');
+autoincrement.initialize(mongoose.Connection);
+
+let Schema = mongoose.Schema;
+
+let CatSchema = new Schema({
+    id: { type:Number, require:true },
+    name: { type:String, require:true },
+    image: { type:String, require:true},
+    since: { type:Date, require:true }
+});
+
+let ProductSchema = new Schema({
+    cat_id : { type:Number, require: true},
+    name : { type:String, require: true},
+    price : { type:Number, require : true },
+    image : { type:String, require: true},
+    description : { type:String, require: true },
+    since : { type: Date, require : true}
+});
+
+let Cat = mongoose.model('category', CatSchema);
+
+ProductSchema.plugin(autoincrement.plugin, 'product');
+let Product = mongoose.model('product', ProductSchema);
+
+module.exports = {
+    Cat,
+    Product
+}
